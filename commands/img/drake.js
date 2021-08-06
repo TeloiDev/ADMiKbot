97d7da56100141129c1ec85b53ad575a9075444c
opuczek <alek.piano@gmail.com>
2021-08-06 16:02:44 +0200

    nowe komendy img

---
+++ commands/img/drake.js
@@ -0,0 +1,55 @@
+const Canvas = require(`canvas`);
+
+module.exports = {
+    name: `drake`,
+    category: `img`,
+    description: "Wygeneruj meme z templatem \"Drake\"",
+    permissions: [],
+    usage: `<prefix> drake <tekst1>, <text2>`,
+    aliases: [`yes-no`, `drk`],
+    async execute(message, args, Discord, client, prefix, colors, command){
+        msg = args.join(` `).split(`,`);
+
+        if (!msg[1]){
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`Podano Złe Argumenty!`)
+            .setColor(colors.incorrectUsage)
+            .setDescription(`Prawidłowe użycie to: \`${command.usage}\``)
+            .setFooter(`Wywołane przez: ${message.author.username}`, message.author.avatarURL({ dynamic: true }))
+            .setTimestamp();
+            return message.channel.send(embed);
+        };
+
+        
+
+        const canvas = Canvas.createCanvas(300, 392);
+        const context = canvas.getContext('2d');
+
+        const background = await Canvas.loadImage('./assets/memes/drake.png');
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+
+        const applyText = (canvas, text) => {
+        
+            let fontSize = 40;
+        
+            do {
+                context.font = `${fontSize -= 10}px sans-serif`;
+            } while (context.measureText(text).width > canvas.width - 100);
+        
+            // Return the result to use in the actual canvas
+            return context.font;
+        };
+        
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+
+        context.font = applyText(canvas, msg);
+        context.fillStyle = '#ffffff';
+        context.fillText(msg, 84, 410);
+
+        
+
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `worst-places.png`);
+        message.channel.send(attachment);
+    }
+}
---
+++ commands/img/worst-places.js
@@ -0,0 +1,43 @@
+const Canvas = require(`canvas`);
+
+module.exports = {
+    name: `worst-places`,
+    category: `img`,
+    description: "Wygeneruj meme z templatem \"Divorce leads children to the worst places\"",
+    permissions: [],
+    usage: `<prefix> worst-places <tekst>`,
+    aliases: [`wp`, `worstplaces`, `divorce`],
+    async execute(message, args, Discord, client, prefix, colors, command){
+        msg = args.join(` `);
+
+        const canvas = Canvas.createCanvas(526, 526);
+        const context = canvas.getContext('2d');
+
+        const background = await Canvas.loadImage('./assets/memes/worst-places.png');
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+
+        const applyText = (canvas, text) => {
+        
+            let fontSize = 40;
+        
+            do {
+                context.font = `${fontSize -= 10}px sans-serif`;
+            } while (context.measureText(text).width > canvas.width - 100);
+        
+            // Return the result to use in the actual canvas
+            return context.font;
+        };
+        
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+
+        context.font = applyText(canvas, msg);
+        context.fillStyle = '#ffffff';
+        context.fillText(msg, 84, 410);
+
+        
+
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `worst-places.png`);
+        message.channel.send(attachment)
+    }
+}
