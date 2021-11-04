09617f0ef828e1d619ca9290aa5586b9fa489257
opuczek <alek.piano@gmail.com>
2021-11-04 13:36:20 +0100

    fix komendy server-info

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS, 
         Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
     ],
 });
 const keepAlive = require(`./server`);
