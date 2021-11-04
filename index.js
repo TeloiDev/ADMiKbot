dfcd736a4c34460ddefdfc70638791d616db662c
opuczek <alek.piano@gmail.com>
2021-11-04 12:51:32 +0100

    naprawa komendy rps poprzez intent

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require(`discord.js`);
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS, 
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ],
 });
 const keepAlive = require(`./server`);
 require(`dotenv`).config();
