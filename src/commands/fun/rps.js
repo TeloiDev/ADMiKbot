0ded7b033cebd7205a755df8db74e3cacb95befc
opuczek <alek.piano@gmail.com>
2021-11-09 22:33:17 +0100

    naprawiłem rps

--- src/commands/fun/rps.js
+++ src/commands/fun/rps.js
@@ -1,17 +1,11 @@
 promptMessage = async function (message, author, time, validReactions) {
-    // We put in the time as seconds, with this it's being transfered to MS
     time *= 1000;
-
-    // For every emoji in the function parameters, react in the good order.
     for (const reaction of validReactions) await message.react(reaction);
 
-    // Only allow reactions from the author, 
-    // and the emoji must be in the array we provided.
     const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
 
-    // And ofcourse, await the reactions
     return message
-        .awaitReactions(filter, { max: 1, time: time})
+        .awaitReactions({filter, max: 1, time: time})
         .then(collected => collected.first() && collected.first().emoji.name);
 }
 
@@ -35,15 +29,11 @@ module.exports = {
 
 
         const m = await message.channel.send({embeds: [embed]});
-        // Wait for a reaction to be added
         const reacted = await promptMessage(m, message.author, 30, chooseArr);
 
-        // Get a random emoji from the array
         const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
 
-        // Check if it's a win/tie/loss
         const result = await getResult(reacted, botChoice);
-        // Clear the reactions
         await m.reactions.removeAll();
 
         embed
