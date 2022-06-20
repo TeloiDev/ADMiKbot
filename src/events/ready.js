c9b62d51eb3b5515b6dc01aeedfabd56cf0b332d
opuczek <alek.piano@gmail.com>
2022-06-20 13:15:38 +0200

    W razie błędu poinformuje o nim na liczeniu

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,30 @@
+const Guild = require("../models/guild");
+const colors = require("../../assets/json/colors.json");
+
 module.exports = async (Discord, client) =>{
     
     console.log(`Hello, World!`);
     client.user.setActivity(`Ryzyko Deszczu`, { type: `PLAYING`});
+
+    Guild.find({}, (err, guilds) => {
+        if (err) {}
+
+        guilds.forEach(async (guildObj) => {
+            if (guildObj.counting) {
+                try {
+
+                    const embed = new Discord.MessageEmbed()
+                    .setColor(colors.incorrectUsage)
+                    .setDescription(`Z powodu liczenie zostało zresetowane do \`${guildObj.counting.count}\``)
+                    .setTimestamp();
+                
+                    client.channels.cache
+                    .find(ch => ch.id === guildObj.counting.countingChannel)
+                    .send({embeds: [embed]})
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        })
+    });
 }
